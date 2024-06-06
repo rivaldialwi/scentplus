1b4405339c32198cd92b7c5a1c5d302f518b086d
unknown <rivaldialwi.com>
2024-06-06 10:12:04 +0700

    cape

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import importlib
 
-st.sidebar.title("Menu")
-menu = st.sidebar.radio("Pilih Menu", ["Prediksi Sentimen", "Laporan"])
+def main():
+    st.sidebar.title("Menu")
+    menu = st.sidebar.radio("Pilih Menu", ["Prediksi Sentimen", "Laporan"])
 
-if menu == "Prediksi Sentimen":
-    st.title("Aplikasi Analisis Sentimen Scentplus - Prediksi Sentimen")
-    module = importlib.import_module('app.py')
-    module.run()
+    if menu == "Prediksi Sentimen":
+        st.title("Aplikasi Analisis Sentimen Scentplus - Prediksi Sentimen")
+        module = importlib.import_module('app')
+        module.run()
 
-elif menu == "Laporan":
-    st.title("Aplikasi Analisis Sentimen Scentplus - Laporan")
-    module = importlib.import_module('laporan.py')
-    module.run()
+    elif menu == "Laporan":
+        st.title("Aplikasi Analisis Sentimen Scentplus - Laporan")
+        module = importlib.import_module('laporan')
+        module.run()
+
+if __name__ == "__main__":
+    main()
