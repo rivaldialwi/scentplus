a3ad1bafdcf0f309b6e9a025c11b5e6a53adde2f
unknown <rivaldialwi.com>
2024-06-06 09:48:02 +0700

    bismillah bisa

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split  # Tambahkan import untuk train_test_split
+from sklearn.model_selection import train_test_split
 
 # Lakukan unduhan NLTK di awal skrip
 nltk.download('stopwords')
-nltk.download('punkt')  # Unduh tokenizer 'punkt' untuk bahasa Indonesia
+nltk.download('punkt')
 
 # Membaca model yang sudah dilatih
 logreg_model = joblib.load("model100.pkl")
@@ -36,28 +36,25 @@ def clean_text(text):
     stop_words = set(stopwords.words('indonesian'))
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
-    text = text.lower()  # Case folding
-    words = word_tokenize(text)  # Tokenizing
-    cleaned_words = [word for word in words if word not in stop_words]  # Stopword removal
-    stemmed_words = [stemmer.stem(word) for word in cleaned_words]  # Stemming
+    text = text.lower()
+    words = word_tokenize(text)
+    cleaned_words = [word for word in words if word not in stop_words]
+    stemmed_words = [stemmer.stem(word) for word in cleaned_words]
     return " ".join(stemmed_words)
 
 # Fungsi untuk melakukan klasifikasi teks
 def classify_text(input_text):
-    # Membersihkan teks input
     cleaned_text = clean_text(input_text)
-    # Mengubah teks input menjadi vektor fitur menggunakan TF-IDF
     input_vector = tfidf_vectorizer.transform([cleaned_text])
-    # Melakukan prediksi menggunakan model
     predicted_label = logreg_model.predict(input_vector)[0]
     return predicted_label
 
-# Streamlit UI
-st.title("Aplikasi Analisis Sentimen scentplus")
-input_text = st.text_input("Masukkan kalimat untuk analisis sentimen:")
-if st.button("Analisis"):
-    if input_text.strip() == "":
-        st.error("Tolong masukkan sentimen terlebih dahulu.")
-    else:
-        result = classify_text(input_text)
-        st.write("Hasil Analisis Sentimen:", result)
+def run():
+    st.title("Aplikasi Analisis Sentimen Scentplus")
+    input_text = st.text_input("Masukkan kalimat untuk analisis sentimen:")
+    if st.button("Analisis"):
+        if input_text.strip() == "":
+            st.error("Tolong masukkan sentimen terlebih dahulu.")
+        else:
+            result = classify_text(input_text)
+            st.write("Hasil Analisis Sentimen:", result)
