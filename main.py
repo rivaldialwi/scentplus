36bf98711dba69ea8fbd7149cc4127d3196bd581
unknown <rivaldialwi.com>
2024-06-06 10:16:23 +0700

    lelah

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 def main():
     st.sidebar.title("Menu")
     menu = st.sidebar.radio("Pilih Menu", ["Prediksi Sentimen", "Laporan"])
@@ -5,13 +7,11 @@ def main():
     if menu == "Prediksi Sentimen":
         st.title("Aplikasi Analisis Sentimen Scentplus - Prediksi Sentimen")
         module = importlib.import_module('app')
-        print("Running app.py...")
         module.run()
 
     elif menu == "Laporan":
         st.title("Aplikasi Analisis Sentimen Scentplus - Laporan")
         module = importlib.import_module('laporan')
-        print("Running laporan.py...")
         module.run()
 
 if __name__ == "__main__":
