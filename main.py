24e3d3ea21a14841ea8f43c05843d4c7b50faf4e
unknown <rivaldialwi.com>
2024-06-06 10:09:12 +0700

    bismillah bisa

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ menu = st.sidebar.radio("Pilih Menu", ["Prediksi Sentimen", "Laporan"])
 
 if menu == "Prediksi Sentimen":
     st.title("Aplikasi Analisis Sentimen Scentplus - Prediksi Sentimen")
-    module = importlib.import_module('app')
+    module = importlib.import_module('app.py')
     module.run()
 
 elif menu == "Laporan":
     st.title("Aplikasi Analisis Sentimen Scentplus - Laporan")
-    module = importlib.import_module('laporan')
+    module = importlib.import_module('laporan.py')
     module.run()
