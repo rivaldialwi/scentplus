7670ddf7f2a3488cc1f9e2e77cf074385df5f62f
unknown <rivaldialwi.com>
2024-06-07 09:08:21 +0700

    bismillah bisa

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
 def main():
-    st.title("Aplikasi Analisis Sentimen Scentplus")
     st.sidebar.title("Menu")
-    st.sidebar.write("Pilih halaman yang ingin Anda akses:")
+    st.sidebar.info("Pilih halaman yang ingin Anda akses.")
+
+    st.title("Aplikasi Analisis Sentimen Scentplus")
+    st.write("""
+        Selamat datang di aplikasi analisis sentimen Scentplus.
+        Silakan pilih menu di sidebar untuk menggunakan aplikasi ini.
+    """)
 
 if __name__ == "__main__":
     main()
